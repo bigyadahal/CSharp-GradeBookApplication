cf87288187a36f4a632142aa8a49734bc468150f
Bigya Dahal <bdahal@uwm.com>
2021-12-30 11:09:51 -0500

    Completed module

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,17 @@
+﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+       
+
+        public StandardGradeBook(string name): base(name) //this will invoke a constructor from BaseGradeBook
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
